814a05dd8977b1c7ef1ac1d777cf507bbff95045
Mr22XX <rayhanma0032@gmail.com>
2024-08-16 17:33:25 +0700

    add regist

--- regist.php
+++ regist.php
@@ -23,16 +23,18 @@
     <div class="flex justify-center items-center mt-52">
         <div class="bg-white/20 backdrop-blur-2xl lg:w-1/4 md:w-80 sm:w-96 h-96 rounded-lg text-white">
             <div class="p-8">
-                <h1 class="text-2xl font-semibold text-center mb-4">Form Registrasi</h1>
-                <label for="username" class="block m-2">Username</label>
-                <input type="text" name="username" class="rounded-sm border-0 bg-transparent border-b-2 border-indigo-600 focus:outline-none focus:ring-0  w-full">
-                <label for="password" class="block m-2">Password</label>
-                <input type="text" name="password" class="rounded-sm border-0 bg-transparent border-b-2 border-indigo-600 focus:outline-none focus:ring-0  w-full">
-                <label for="password2" class="block m-2">Konfirmasi Password</label>
-                <input type="text" name="password2" class="rounded-sm border-0 bg-transparent border-b-2 border-indigo-600 focus:outline-none focus:ring-0  w-full">
-                <div class="flex justify-end p-5">
-                    <button type="submit" class="bg-indigo-600 h-10 w-16 rounded-md ">Daftar</button>
-                </div>
+                <form action="" method="post">
+                    <h1 class="text-2xl font-semibold text-center mb-4">Form Registrasi</h1>
+                    <label for="username" class="block m-2">Username</label>
+                    <input type="text" name="username" class="rounded-sm border-0 bg-transparent border-b-2 border-indigo-600 focus:outline-none focus:ring-0  w-full">
+                    <label for="password" class="block m-2">Password</label>
+                    <input type="text" name="password" class="rounded-sm border-0 bg-transparent border-b-2 border-indigo-600 focus:outline-none focus:ring-0  w-full">
+                    <label for="password2" class="block m-2">Konfirmasi Password</label>
+                    <input type="text" name="password2" class="rounded-sm border-0 bg-transparent border-b-2 border-indigo-600 focus:outline-none focus:ring-0  w-full">
+                    <div class="flex justify-end p-5">
+                        <button type="submit" class="bg-indigo-600 h-10 w-16 rounded-md ">Daftar</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
