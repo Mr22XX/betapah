2f35db5f2a6a49c702274c33b164676b93aa13ec
Mr22XX <rayhanma0032@gmail.com>
2024-08-16 17:32:13 +0700

    add regist

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "sql204.infinityfree.com";
-$username = "if0_37040903";
-$password = "Ozk1Sx5Tt03E";
-$dbName = "if0_37040903_betapah";
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbName = "betapah";
 
 $conn = mysqli_connect($host, $username, $password, $dbName);
 if(!$conn){
---
+++ regist.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bengkulu Tanpa Sampah</title>
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.css" rel="stylesheet" />
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-[url('assets/bg.jpg')] bg-center bg-cover bg-no-repeat h-screen">
+
+    <!-- Promotion -->
+    <div class="bg-indigo-600 px-4 py-3 text-white">
+        <p class="text-center text-sm font-medium">
+          Ads 🎉
+          <a href="#" class="inline-block underline">Your Advertisement Here !</a>
+        </p>
+    </div>
+    <!-- End Promotion -->
+
+    <!-- Form -->
+
+    <div class="flex justify-center items-center mt-52">
+        <div class="bg-white/20 backdrop-blur-2xl lg:w-1/4 md:w-80 sm:w-96 h-96 rounded-lg text-white">
+            <div class="p-8">
+                <h1 class="text-2xl font-semibold text-center mb-4">Form Registrasi</h1>
+                <label for="username" class="block m-2">Username</label>
+                <input type="text" name="username" class="rounded-sm border-0 bg-transparent border-b-2 border-indigo-600 focus:outline-none focus:ring-0  w-full">
+                <label for="password" class="block m-2">Password</label>
+                <input type="text" name="password" class="rounded-sm border-0 bg-transparent border-b-2 border-indigo-600 focus:outline-none focus:ring-0  w-full">
+                <label for="password2" class="block m-2">Konfirmasi Password</label>
+                <input type="text" name="password2" class="rounded-sm border-0 bg-transparent border-b-2 border-indigo-600 focus:outline-none focus:ring-0  w-full">
+                <div class="flex justify-end p-5">
+                    <button type="submit" class="bg-indigo-600 h-10 w-16 rounded-md ">Daftar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+     
+
+</body>
+<script src="main.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/flowbite@2.4.1/dist/flowbite.min.js"></script>
+
+</html>
