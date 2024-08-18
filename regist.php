6fda142fa327c44ac48ec847291cfefb33708cc9
Mr22XX <rayhanma0032@gmail.com>
2024-08-19 00:40:53 +0700

    add regist

--- regist.php
+++ regist.php
@@ -1,3 +1,63 @@
+<?php
+
+include "conn.php";
+
+
+
+
+
+if(isset($_POST['regist'])){
+
+    $username = strtolower(stripcslashes($_POST['username']));
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $password2 = mysqli_real_escape_string($conn, $_POST['password2']);
+        if($password === $password2){
+            $cekUser = mysqli_query($conn, "SELECT username from user WHERE username = '$username'");
+        if(mysqli_fetch_assoc($cekUser)){
+            echo "
+            <script>
+            alert('Username sudah terdaftar')
+            window.location.href = 'regist.php';
+            </script>
+            ";
+            return false;
+        }
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $query = "INSERT INTO user (username, password) VALUES ('$username', '$password')";  
+        $hasil = mysqli_query($conn, $query);
+
+            if($hasil){
+                echo "
+                <script>
+                alert('Registrasi Berhasil')
+                </script>
+                ";
+            } else {
+                echo "
+                <script>
+                alert('Registrasi Gagal')
+                </script>
+                ";
+            }
+        }
+        else{
+            echo "
+            <script>
+            alert('Password yang dimasukkan berbeda')
+            </script>
+        ";
+        }
+    
+
+
+
+  
+}
+
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -28,11 +88,11 @@
                     <label for="username" class="block m-2">Username</label>
                     <input type="text" name="username" class="rounded-sm border-0 bg-transparent border-b-2 border-indigo-600 focus:outline-none focus:ring-0  w-full">
                     <label for="password" class="block m-2">Password</label>
-                    <input type="text" name="password" class="rounded-sm border-0 bg-transparent border-b-2 border-indigo-600 focus:outline-none focus:ring-0  w-full">
+                    <input type="password" name="password" class="rounded-sm border-0 bg-transparent border-b-2 border-indigo-600 focus:outline-none focus:ring-0  w-full">
                     <label for="password2" class="block m-2">Konfirmasi Password</label>
-                    <input type="text" name="password2" class="rounded-sm border-0 bg-transparent border-b-2 border-indigo-600 focus:outline-none focus:ring-0  w-full">
+                    <input type="password" name="password2" class="rounded-sm border-0 bg-transparent border-b-2 border-indigo-600 focus:outline-none focus:ring-0  w-full">
                     <div class="flex justify-end p-5">
-                        <button type="submit" class="bg-indigo-600 h-10 w-16 rounded-md ">Daftar</button>
+                        <button type="submit" name="regist" class="bg-indigo-600 h-10 w-16 rounded-md ">Daftar</button>
                     </div>
                 </form>
             </div>
